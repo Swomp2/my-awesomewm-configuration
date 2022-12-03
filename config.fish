5d9fa1278daeec678fefadff60c97cd839772dff
Swomp2 <sviotidiot@gmail.com>
2022-12-03 23:59:01 +0300

    first commit

---
+++ config.fish
@@ -0,0 +1,10 @@
+if status is-login
+    startx   
+end
+
+export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_STYLE_OVERRIDE=kvantum
+set PATH "$PATH:/home/swomp/.local/bin"
+set PATH "$PATH:$HOME/.cargo/bin"
+alias poweroff="sudo poweroff"
+alias reboot="sudo reboot"
